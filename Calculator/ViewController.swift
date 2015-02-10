f7b8ab246b0fbd0d1aea4982b252a0989e9d40a9
Alexey Rakeev <alexey@rakeev.name>
2015-02-11 03:52:22 +0400

    Added basic controls

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,4 +1,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var display: UILabel!
+    var displayEmpty = true
+
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if displayEmpty {
+            display.text = digit
+            displayEmpty = false
+        } else {
+            display.text = display.text! + digit
+        }
+    }
 }
